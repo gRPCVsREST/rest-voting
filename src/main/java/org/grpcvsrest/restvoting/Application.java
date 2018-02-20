310ef5be52c8869648242e066d3d3dd68cebabd4
Mykyta Protsenko <mykyta@protsenko.com>
2018-02-19 20:24:13 -0800

    Added span-adjuster

--- src/main/java/org/grpcvsrest/restvoting/Application.java
+++ src/main/java/org/grpcvsrest/restvoting/Application.java
@@ -1,9 +1,11 @@
 package org.grpcvsrest.restvoting;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.sleuth.SpanAdjuster;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +14,12 @@ import org.springframework.web.client.RestTemplate;
 @EnableCircuitBreaker
 @EnableHystrix
 public class Application {
+
+    @Bean
+    public SpanAdjuster customSpanAdjuster(@Value("${spring.application.name}") String appName) {
+        return span -> span.toBuilder().name("#" + appName + "/" + span.getName().replace("http:/", "")).build();
+    }
+
 
     @Bean
     public RestTemplate restTemplate() {
